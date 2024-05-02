0e8a50396e1c2ec552564ea98109e7009cb6e594
levrx <jvcke@icloud.com>
2024-05-02 21:03:45 +0100

    yay

--- functions/api/bing/wallpaper/index.js
+++ functions/api/bing/wallpaper/index.js
@@ -1,20 +1,13 @@
 export async function onRequest(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
-  const res = await fetch(`https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=5`);
-  const bing_data = await res.json();
-  const return_data = {
+  const info = JSON.stringify({
     "status": true,
-    "message": "Successful operation",
-    "data": bing_data.images
-  }
-  const info = JSON.stringify(return_data);
-  return new Response(info);
+    "message": "Successful operation"
+  });
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Background-Color": "#40474d" // Set background color here
+  };
+
+  return new Response(info, { headers });
 }
